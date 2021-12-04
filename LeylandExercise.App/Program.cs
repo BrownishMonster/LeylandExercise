068e0570ae241ece59b05e7733a238adb4e5890b
BrownishMonster <56352230+BrownishMonster@users.noreply.github.com>
2021-12-04 11:01:53 +0000

    Add calculator to console

--- LeylandExercise.App/Program.cs
+++ LeylandExercise.App/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LeylandExercise.App;
+
+IPerfectSquareCalculator calculator = new CustomPerfectSquareCalculator();
+var defaultColor = Console.ForegroundColor;
+
+Console.WriteLine("Write an integer here, and we will calculate if it is a perfect Square!");
+
+string line = default;
+while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+{
+    if (int.TryParse(line, out var value))
+    {
+        Console.WriteLine($"\nIs {value} a perfect square?");
+        Console.WriteLine(calculator.IsPerfectSquare(value) ? "\tYes" : "\tNo");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Please enter an integer!");
+        Console.ForegroundColor = defaultColor;
+    }
+    Console.WriteLine(new string('-', 25));
+}
