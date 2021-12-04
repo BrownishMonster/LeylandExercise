d3208ecbdb018e8913cbed869b07ea1da65ef568
BrownishMonster <56352230+BrownishMonster@users.noreply.github.com>
2021-12-04 10:09:22 +0000

    Add Testcases

--- PerfectSquareCalculatorTests/PerfectSquareTests.cs
+++ PerfectSquareCalculatorTests/PerfectSquareTests.cs
@@ -1,13 +1,37 @@
+using System;
 using Xunit;
 
 namespace LeylandExercise.Tests
 {
     public class PerfectSquareTests
     {
-        [Fact]
-        public void Test1()
+        private void TestNumber(int number, bool expected)
         {
-
+            throw new NotImplementedException();
         }
+
+        [Fact]
+        public void When_NegativeOne_Expect_False()
+            => TestNumber(0, false);
+
+        [Fact]
+        public void When_Zero_Expect_True()
+            => TestNumber(0, true);
+
+        [Fact]
+        public void When_Three_Expect_False()
+            => TestNumber(3, false);
+
+        [Fact]
+        public void When_Four_Expect_True()
+            => TestNumber(4, true);
+
+        [Fact]
+        public void When_TwentyFive_Expect_True()
+            => TestNumber(25, true);
+
+        [Fact]
+        public void When_TwentySix_Expect_False()
+            => TestNumber(26, false);
     }
 }
